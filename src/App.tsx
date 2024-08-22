b0f37a3998299af3ddf81e9aa88ff01244d81bf8
taehongkim <public.bluemiv@gmail.com>
2024-08-22 21:03:52 +0900

    feat: path aliasing 설정 #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Title } from '@/components';
+
 export default function App() {
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center">
       <div className="max-w-[640px] w-full max-h-[70vh] h-full min-h-[300px] bg-white rounded mx-auto p-10">
-        <h1 className="font-bold text-center text-3xl">React To Do App</h1>
+        <Title>To Do List</Title>
       </div>
     </div>
   );
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+import { TPropsWithChildren } from '../types';
+
+export default function Title({ children }: TPropsWithChildren) {
+  return <h1>{children}</h1>;
+}
