150bbd6ecfadf1989b4f97a381d2e63a28fb9316
taehongkim <public.bluemiv@gmail.com>
2024-08-25 21:04:53 +0900

    style: 반응형 디자인 #17

--- src/hooks/screen.ts
+++ src/hooks/screen.ts
@@ -4,16 +4,18 @@ export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    const checkIsMobile = () => {
+      const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+      const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        userAgent,
+      );
+      setIsMobile(mobile || ('ontouchstart' in window && window.innerWidth <= 1024));
+    };
+
     checkIsMobile();
     window.addEventListener('resize', checkIsMobile);
     return () => window.removeEventListener('resize', checkIsMobile);
   }, []);
-
-  const checkIsMobile = () => {
-    const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-    const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-    setIsMobile(mobile || 'ontouchstart' in window);
-  };
 
   return isMobile;
 };
